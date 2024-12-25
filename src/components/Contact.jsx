edbf574ba48d30e9b6150bf1d0a49b3c00c9b192
hemanthkt <hmkt1503@gmail.com>
2024-12-25 20:06:57 +0530

    updates contacts section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { CONTACT } from "../constants";
+import { motion } from "framer-motion";
 function Contact() {
   return (
-    <div className="border-b border-neutral-900 pb-20">
+    <motion.div
+      whileInView={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0, y: -10 }}
+      transition={{ duration: 1 }}
+      className="border-b border-neutral-900 pb-20"
+    >
       <h2 className="my-10 text-center text-4xl">Get in Touch </h2>
       <div className="text-center tracking-tighter">
         <p className="my-4">{CONTACT.address}</p>
         <p className="my-4">{CONTACT.phoneNo}</p>
         <a href="#"></a>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
