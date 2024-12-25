7cd91d4d0fdd81095bf2c5e81ed2326d1b2d8a98
hemanthkt <hmkt1503@gmail.com>
2024-12-25 17:26:17 +0530

    adds project, adds motoin framer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
         <About />
         <Technologies></Technologies>
         <Experience></Experience>
+        <Projects></Projects>
+        <Contact></Contact>
       </div>
     </div>
   );
