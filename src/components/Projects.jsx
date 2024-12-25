f5471d7ec3e34f36309d34a7e1365b676c8a0823
hemanthkt <hmkt1503@gmail.com>
2024-12-25 20:00:56 +0530

    adds framer moton in experiece, projecs,technology, about sections

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import { PROJECTS } from "../constants";
-
+import { motion } from "framer-motion";
 function Projects() {
   return (
     <div className="border-b border-neutral-900">
-      <h2 className="my-20 text-center text-4xl">Projects</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Projects
+      </motion.h2>
 
       <div>
         {PROJECTS.map((project, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <div className="w-full lg:w-1/4">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 0.5 }}
+              className="w-full lg:w-1/4"
+            >
               <img className="mb-6 rounded" src={project.image}></img>
-            </div>
-            <div className="w-full lg:ml-5 max-w-xl lg:w-3/4">
+            </motion.div>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 0.5 }}
+              className="w-full lg:ml-5 max-w-xl lg:w-3/4"
+            >
               <h6 className="mb-2 font-semibold">{project.title}</h6>
               <p className="mb-4">{project.description}</p>
               {project.technologies.map((tech, index) => (
@@ -23,7 +40,7 @@ function Projects() {
                   {tech}
                 </span>
               ))}
-            </div>
+            </motion.div>
           </div>
         ))}
       </div>
